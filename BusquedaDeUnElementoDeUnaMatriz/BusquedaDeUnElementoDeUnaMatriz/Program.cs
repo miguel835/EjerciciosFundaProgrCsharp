bf72bf56a33bdc3b757a22d51aca1f973c9a69f1
you@example.com <you@example.com>
2024-12-07 19:14:16 -0500

    Se agregan busqueda de un elemento de una matriz y suma de elementos de una matriz

---
+++ BusquedaDeUnElementoDeUnaMatriz/BusquedaDeUnElementoDeUnaMatriz/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Crear una matriz bidimensional de enteros, solicitar al usuario un valor entero a buscar, recorrer la matriz y buscar
+el elemento especificado, indicar si el elemento se encuentra en la matriz y en que posición7*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaDeUnElementoDeUnaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Declaración de variables
+                int[,] matriz;
+                int filas, columnas, i = 0, j, limiteRandom, numeroBusqueda, contador = 0;
+                Random aleatorio = new Random();
+                //Configuración visual de la consola
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Clear();
+                //Titulo del programa
+                Console.SetCursorPosition(10, 0);
+                Console.WriteLine("Programa para buscar un elemento en una matriz");
+                //Configuración de la matriz
+                Console.Write("Ingrese el numero de filas: ");
+                filas = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el numero de columnas: ");
+                columnas = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el numero limite para el llenado aleatorio: ");
+                limiteRandom = int.Parse(Console.ReadLine());
+                //Creación de la matriz
+                matriz = new int[filas, columnas];
+                //Recorrido, llenado aleatorio de la matriz 
+                while (i < filas)
+                {
+                    j = 0;
+                    while (j < columnas)
+                    {
+                        matriz[i, j] = aleatorio.Next(1, limiteRandom);
+                        j++;
+                    }
+                    i++;
+                }
+                Console.Write("Ingrese un numero a buscar en la matriz: ");
+                numeroBusqueda = int.Parse(Console.ReadLine());
+                //Busqueda del numero en la matriz
+                for (i = 0; i < filas; i++)
+                {
+                    for (j = 0; j < columnas; j++)
+                    {
+                        if (matriz[i, j] == numeroBusqueda)
+                        {
+                            Console.WriteLine($"El numero {numeroBusqueda} ha sido encontrado en la posición [{i},{j}]");
+                            contador++;
+                        }
+                    }
+                }
+                if (contador == 0) Console.WriteLine($"No hay ningun numero {numeroBusqueda} en la matriz");
+            } catch { Console.WriteLine("Dato ingresado no valido"); }
+        }
+    }
+}
---
+++ SumaDeElementosDeUnaMatriz/SumaDeElementosDeUnaMatriz/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Crear una matriz bidimensional de enteros, solicitar al usuario el numero de filas y columnas de la matriz, 
+recorrer la matriz e ingresar un valor entero en cada posición, imprimir cada elemento de la matriz, 
+imprimir la suma total en consola*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaDeElementosDeUnaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Declaración de variables
+                int[,] matriz;
+                int i = 0, j = 0, filas, columnas, suma = 0;
+                //Configuración visual de la consola
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Clear();
+                //Titulo
+                Console.SetCursorPosition(10, 0);
+                Console.WriteLine("Programa para obtener la suma total de una matriz");
+                //Ingreso del numero de filas y columnas
+                Console.Write("Ingrese el numero de filas: ");
+                filas = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el numero de columnas: ");
+                columnas = int.Parse(Console.ReadLine());
+                //Establecemos el tamaño de la matriz
+                matriz = new int[filas, columnas];
+                //Recorrido de la matriz
+                //Recorrido de las filas
+                while (i < filas)
+                {
+                    j = 0;
+                    //Recorrido de las columnas
+                    while (j < columnas)
+                    {
+                        Console.Write($"Ingrese el valor para la posición matriz[{i},{j}]: ");
+                        matriz[i, j] = int.Parse(Console.ReadLine());
+                        j++;
+                    }
+                    i++;
+                }
+                //Impresión de la matriz
+                //Recorrido de las filas
+                for (i = 0; i < filas; i++)
+                {
+                    //Recorrido de las columnas
+                    for (j = 0; j < columnas; j++)
+                    {
+                        //Se imprime el elemento de la matriz y se realiza la suma de cada elemento
+                        suma += matriz[i, j];
+                        Console.Write(matriz[i, j]);
+                    }
+                    Console.Write("\n");
+                }
+                //Impresion de la suma de los elementos de la matriz
+                Console.WriteLine($"La suma de todos los elementos es: {suma}");
+            } catch { Console.WriteLine("Dato ingresado erroneo"); }
+        }
+    }
+}
